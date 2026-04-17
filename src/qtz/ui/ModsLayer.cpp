dae7575217e0068c4c1e45c3a6f1e8c32de95032
SILVERSTAR <silverstar.apollo@gmail.com>
2026-04-18 00:28:01 +0800

    scroll layer positioned correctly

--- src/qtz/ui/ModsLayer.cpp
+++ src/qtz/ui/ModsLayer.cpp
@@ -45,71 +45,60 @@ bool ModsLayer::init() {
     auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
 
     m_bg = cocos2d::CCSprite::create("game_bg_01_001.png");
-    if (m_bg) {
-        m_bg->setScale(1.125f);
-        m_bg->setAnchorPoint(cocos2d::CCPoint(0.f, 0.5f));
-        auto& texRect = m_bg->getTextureRect();
-        cocos2d::CCRect newTexRect;
-        newTexRect.setRect(0.f, 0.f, texRect.size.width * 2, texRect.size.height);
-        m_bg->setTextureRect(newTexRect);
-        m_bg->setColor(cocos2d::ccColor3B(0, 102, 255));
-        m_bg->setPosition(
-            cocos2d::CCPoint(
-                0.f,
-                winSize.height / 2.f
-            )
-        );
+    m_bg->setScale(1.125f);
+    m_bg->setAnchorPoint(cocos2d::CCPoint(0.f, 0.5f));
+    auto& texRect = m_bg->getTextureRect();
+    cocos2d::CCRect newTexRect;
+    newTexRect.setRect(0.f, 0.f, texRect.size.width * 2, texRect.size.height);
+    m_bg->setTextureRect(newTexRect);
+    m_bg->setColor(cocos2d::ccColor3B(0, 102, 255));
+    m_bg->setPosition(
+        cocos2d::CCPoint(
+            0.f,
+            winSize.height / 2.f
+        )
+    );
 
-        this->addChild(m_bg);
-    }
+    this->addChild(m_bg);
 
     auto bgPlate = cocos2d::extension::CCScale9Sprite::create("GJ_square02.png");
-    if (bgPlate) {
-        bgPlate->setContentSize(cocos2d::CCPoint(400.f, 250.f));
-        bgPlate->setPosition(winSize / 2.f);
-        this->addChild(bgPlate);
+    bgPlate->setContentSize(cocos2d::CCPoint(400.f, 250.f));
+    bgPlate->setPosition(winSize / 2.f);
+    this->addChild(bgPlate);
 
-        auto text = cocos2d::CCLabelBMFont::create("Addons", "bigFont.fnt");
-        if (text) {
-            bgPlate->addChild(text);
-            auto* textParent = text->getParent();
-            text->setScale(0.8f);
-            text->setPositionX(textParent->getContentWidth() / 2.f);
-            text->setPositionY(textParent->getContentHeight() - 3.f);
-            text->setAnchorPoint(cocos2d::CCPoint(0.5f, 1.f));
-        }
+    auto text = cocos2d::CCLabelBMFont::create("Addons", "bigFont.fnt");
+    bgPlate->addChild(text);
+    text->setScale(0.8f);
+    text->setPositionX(text->getParent()->getContentWidth() / 2.f);
+    text->setPositionY(text->getParent()->getContentHeight() - 3.f);
+    text->setAnchorPoint(cocos2d::CCPoint(0.5f, 1.f));
 
-        auto scrollLayer = geode::ScrollLayer::create(cocos2d::CCSize(200, 200));
-        if (scrollLayer) {
-            bgPlate->addChild(scrollLayer);
-            scrollLayer->getChildByID("content-layer")->addChild(cocos2d::CCSprite::create("fallbackModLogo.png"_spr));
-            scrollLayer->setPosition(scrollLayer->getContentSize() / 2.f);
-        }
-    }
+    auto scrollLayer = geode::ScrollLayer::create(cocos2d::CCSize(300, 200));
+    bgPlate->addChild(scrollLayer);
+    scrollLayer->getChildByID("content-layer")->addChild(cocos2d::CCSprite::create("fallbackModLogo.png"_spr));
+    scrollLayer->setPositionX((scrollLayer->getParent()->getContentWidth() / 2.f) - (scrollLayer->getContentWidth() / 2.f));
+    scrollLayer->setPositionY(15.f);
+    // scrollLayer->setPosition(bgPlate->getContentSize() / 2.f);
     
     auto menu = cocos2d::CCMenu::create();
-    if (menu) {
-        auto closeBtn = CCMenuItemSpriteExtra::create(
-            cocos2d::CCSprite::createWithSpriteFrameName("GJ_arrow_03_001.png"), this,
-            menu_selector(ModsLayer::goBackToLastScene)
-        );
+    auto closeBtn = CCMenuItemSpriteExtra::create(
+        cocos2d::CCSprite::createWithSpriteFrameName("GJ_arrow_03_001.png"), this,
+        menu_selector(ModsLayer::goBackToLastScene)
+    );
 
-        if (closeBtn) {
-            menu->setContentSize(closeBtn->getContentSize());
-            menu->addChild(closeBtn);
-            closeBtn->setPosition(closeBtn->getParent()->getContentSize() / 2.f);
+    menu->setContentSize(closeBtn->getContentSize());
+    menu->addChild(closeBtn);
+    closeBtn->setPosition(closeBtn->getParent()->getContentSize() / 2.f);
 
-            float padding = 5.f;
-            menu->setPosition(
-                cocos2d::CCPoint(
-                    padding,
-                    winSize.height - menu->getContentSize().height - padding
-                )
-            );
+    float padding = 5.f;
+    menu->setPosition(
+        cocos2d::CCPoint(
+            padding,
+            winSize.height - menu->getContentSize().height - padding
+        )
+    );
 
-            this->addChild(menu);
-        }
-    }
+    this->addChild(menu);
 
     return true;
 }
